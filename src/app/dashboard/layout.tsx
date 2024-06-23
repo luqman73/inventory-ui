df8962b905a3767de22c9d00d139cabd88eeb4fd
luqman73 <luqmanhazli@gmail.com>
2024-06-23 19:35:45 +0800

    remove unessassary scroll bar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,13 +9,13 @@ export default function registerLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div>
+        <div className="h-screen flex flex-col">
             <Navbar/>
-            <div className="flex min-h-screen">
+            <div className="flex flex-1 overflow-hidden">
                 <div className="w-64">
                     <Sidebar/>
                 </div>
-                <div className="flex-1 p-6">
+                <div className="flex-1 p-6 overflow-auto">
                     {children}
                 </div>
             </div>
