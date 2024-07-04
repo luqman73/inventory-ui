19fc73e6cbdfb3467862e9324b963e7c7b4adfd9
luqman73 <luqmanhazli@gmail.com>
2024-07-04 11:15:26 +0800

    format document indentation

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -37,7 +37,7 @@ const LoginPage = () => {
             Sign in to your account
           </h2>
         </div>
-        
+
         <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
           <form onSubmit={handleLogin} className="space-y-6">
             <div>
@@ -46,11 +46,11 @@ const LoginPage = () => {
               </label>
               <div className="mt-2">
                 <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="block w-full rounded-md border-0 py-1.5 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
               </div>
             </div>
             <div>
@@ -67,12 +67,12 @@ const LoginPage = () => {
                   />
                 </div>
               </div>
-            </div>   
+            </div>
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-            Login
+              Login
             </button>
           </form>
         </div>
