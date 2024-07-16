1695f60b80b36ed4e6173cc14e9f7fb287421421
luqman73 <luqmanhazli@gmail.com>
2024-07-16 16:20:13 +0800

    WIP on add product - bug at first submit

--- src/components/contents/ColorDropdown.tsx
+++ src/components/contents/ColorDropdown.tsx
@@ -2,11 +2,10 @@
 import { useState, useEffect } from "react";
 import api from "src/api/api";
 
-const ColorDropdown = () => {
+const ColorDropdown = ({ color_id, onColorChange}) => {
     const [colors, setColors] = useState([]);
 
     useEffect(() => {
-        //Fetch colors from the API
         const fetchColors = async () => {
             try {
                 const response = await api.get('/colors');
@@ -23,9 +22,17 @@ const ColorDropdown = () => {
         <div className="inline-block text-left">
             <div className="flex items-center">
                 <div className="mt-2 bg-white border rounded-md shadow-md">
-                    <select name="color" id="color" className="form-select">
-                        {colors.map((color, index) => (
-                            <option key={index} value={color.name}>{color.name}</option>
+                    <select
+                        name="color_id"
+                        id="color"
+                        className="form-select"
+                        value={color_id}
+                        onChange={(e) => onColorChange(e.target.value)}
+                    >
+                        {colors.map((color) => (
+                            <option key={color.id} value={color.id}>
+                                {color.name}
+                            </option>
                         ))}
                     </select>
                 </div>
